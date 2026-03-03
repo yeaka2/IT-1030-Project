fa5b61890893922477e6bec2afe8b12ea16434be
yeaka2 <zhiyuanxie0314@gmail.com>
2026-03-03 23:11:21 +0800

    cal4 and 5

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # IT-1030-Project
+
+This project includes a compound interest calculator and additional utility pages. A new "Savings Calculator" (calculator4) has been added, letting users compute future savings with monthly deposits. The savings calculator focuses solely on deposits now; a separate "Expense Calculator" (calculator5) has been added to compute total outlay over time based on monthly expense and years. The savings calculator remains available without the expense field.
+
---
+++ navactions.js
@@ -0,0 +1,24 @@
+document.querySelector('.nav-button1').addEventListener('mouseup',function(){
+    document.location.href='index.html'
+})
+
+document.querySelector('.nav-button2').addEventListener('mouseup',function(){
+    document.location.href='index.html'
+})
+
+document.querySelector('.nav-button3').addEventListener('mouseup',function(){
+    document.location.href='index.html'
+})
+
+document.querySelector('.nav-button4').addEventListener('mouseup',function(){
+    document.location.href='calculator4.html'
+})
+
+document.querySelector('.nav-button5').addEventListener('mouseup',function(){
+    document.location.href='calculator5.html'
+})
+
+document.querySelector('.nav-button6').addEventListener('mouseup',function(){
+    document.location.href='Personal.html'
+})
+
