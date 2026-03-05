33ce5111ed78f6adf7b91522ddfaf3954631dc61
jinzezhu0609-ops <jinzezhu0609@gmail.com>
2026-03-05 11:47:43 +0800

    Update navactions.js
    Fix  two link issues

--- navactions.js
+++ navactions.js
@@ -3,11 +3,11 @@ document.querySelector('.nav-button1').addEventListener('mouseup',function(){
 })
 
 document.querySelector('.nav-button2').addEventListener('mouseup',function(){
-    document.location.href='index.html'
+    document.location.href='exchange-calculator.html'
 })
 
 document.querySelector('.nav-button3').addEventListener('mouseup',function(){
-    document.location.href='index.html'
+    document.location.href='loan.html'
 })
 
 document.querySelector('.nav-button4').addEventListener('mouseup',function(){
