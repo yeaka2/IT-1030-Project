3dd932ca7130553407ab5df50811105ff86d528c
93enustaH <d85780855@gmail.com>
2026-03-28 23:17:28 +0800

    Add files via upload

---
+++ navactions.js
@@ -0,0 +1,25 @@
+document.querySelector('.nav-button1').addEventListener('mouseup',function(){
+    document.location.href='compound-interest-calculator.html'
+})
+
+document.querySelector('.nav-button2').addEventListener('mouseup',function(){
+    document.location.href='exchange-calculator.html'
+})
+
+document.querySelector('.nav-button3').addEventListener('mouseup',function(){
+    document.location.href='loan.html'
+})
+
+document.querySelector('.nav-button4').addEventListener('mouseup',function(){
+    document.location.href='saving.html'
+})
+
+document.querySelector('.nav-button5').addEventListener('mouseup',function(){
+    document.location.href='expense.html'
+})
+
+document.querySelector('.nav-button6').addEventListener('mouseup',function(){
+    document.location.href='Personal.html'
+})
+
+
